76279594c0c076f028e81701c697162e6b00cf57
Andreas Perhab <bigbear.ap@gmail.com>
2013-10-12 16:15:52 +0200

    read content from repository

--- src/main/java/Tryout.java
+++ src/main/java/Tryout.java
@@ -1,12 +1,21 @@
+import java.awt.BorderLayout;
+import java.awt.Image;
 import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.jcr.Binary;
 import javax.jcr.Node;
+import javax.jcr.PathNotFoundException;
 import javax.jcr.Repository;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.jcr.ValueFormatException;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 import org.modeshape.common.collection.Problems;
 import org.modeshape.jcr.ModeShapeEngine;
@@ -24,7 +33,32 @@ public class Tryout {
 		
 		debugTree(root);
 
+		readContent(session);
 
+
+	}
+
+	private static void readContent(Session session) {
+		try {
+			Node doc = session.getNode("/files/firefox-logo.png");
+			Node imageContent = doc.getNode("jcr:content");
+			Binary content = imageContent.getProperty("jcr:data").getBinary();
+			InputStream is = content.getStream();
+			 
+			Image image = ImageIO.read(is);
+			JFrame frame = new JFrame();
+			JLabel label = new JLabel(new ImageIcon(image));
+			frame.getContentPane().add(label, BorderLayout.CENTER);
+			frame.pack();
+			frame.setVisible(true);
+			is.close();
+		} catch (RepositoryException e) {
+			System.out.println("There was an error.");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("There was an error.");
+			e.printStackTrace();
+		}
 	}
 
 	private static void debugTree(Node root) {
@@ -57,7 +91,7 @@ public class Tryout {
 			InputStream stream =
 			    new BufferedInputStream(Tryout.class.getResourceAsStream("firefox-logo.png"));
 			// Create an 'nt:file' node at the supplied path ...
-			Node fileNode = filesNode.addNode("firefox-logo.jpg","nt:file");
+			Node fileNode = filesNode.addNode("firefox-logo.png","nt:file");
 	
 			// Upload the file to that node ...
 			Node contentNode = fileNode.addNode("jcr:content", "nt:resource");
